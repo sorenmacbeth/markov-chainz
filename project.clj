bc10443adfe92480396b8251e1ebf0ef2327fcfd
Soren Macbeth <soren@dopeness.org>
2014-03-26 23:00:10 -0700

    initial comet

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject markov-chainz "0.1.0-SNAPSHOT"
+  :description "simple library for building text-based markov chains"
+  :url "https://github.com/sorenmacbeth/markov-chainz"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :source-paths ["src/clj"])
